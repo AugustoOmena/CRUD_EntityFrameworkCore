8cfc75f7a69df71d9f87edbc102641d3d8d86e8d
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-29 20:43:01 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_EntityFrameworkCore/Models/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_EntityFrameworkCore.Models
+{
+    public class UserDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer(@"Data Source=LAPTOP-OPH5U10J\SQLEXPRESS;Initial Catalog=CrudDemo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+    }
+}
---
+++ CRUD_EntityFrameworkCore/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace CRUD_EntityFrameworkCore.Models
+{
+    public class UserModel
+    {
+        [Key]
+        [Column(TypeName = "int")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        [DisplayName("Primeiro Nome")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        [DisplayName("Ultimo Nome")]
+        public String LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        [DisplayName("Contact No")]
+        public String ContactNo { get; set; } = "0";
+    }
+}
