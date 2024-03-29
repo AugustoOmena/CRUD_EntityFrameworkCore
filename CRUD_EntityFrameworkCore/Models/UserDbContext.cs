b597914806b3b5eb1526de2ed5523b5fa51f9b23
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-29 20:46:19 -0300

    Create Project, Install NuGet Packages, Add DvContext Class, Add Connection String, Add Db Set Propriety.

--- CRUD_EntityFrameworkCore/Models/UserDbContext.cs
+++ CRUD_EntityFrameworkCore/Models/UserDbContext.cs
@@ -6,5 +6,6 @@ namespace CRUD_EntityFrameworkCore.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder options) =>
             options.UseSqlServer(@"Data Source=LAPTOP-OPH5U10J\SQLEXPRESS;Initial Catalog=CrudDemo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+
     }
 }
