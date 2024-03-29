0d58ef5962e9287b647b1df15b2a1e1e3611376d
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-29 20:48:49 -0300

    Add DbSetProperty.

--- CRUD_EntityFrameworkCore/Models/UserDbContext.cs
+++ CRUD_EntityFrameworkCore/Models/UserDbContext.cs
@@ -6,6 +6,8 @@ namespace CRUD_EntityFrameworkCore.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder options) =>
             options.UseSqlServer(@"Data Source=LAPTOP-OPH5U10J\SQLEXPRESS;Initial Catalog=CrudDemo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
-
+   
+        public DbSet<UserModel> Users { get; set; }
     }
+
 }
