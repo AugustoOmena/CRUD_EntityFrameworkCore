7bd5d328516ed43a3f132309ac5e8cc45ed65e34
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-29 21:09:22 -0300

    Add Controller in the Project.

---
+++ CRUD_EntityFrameworkCore/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using CRUD_EntityFrameworkCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_EntityFrameworkCore.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserDbContext _context;
+        public UserController(UserDbContext userDbContext) 
+        {
+            _context = userDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
